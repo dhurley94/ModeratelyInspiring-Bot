9d13d455a1579a8cff46800c5d386436604bc814
Dave <dhurley@singlehop.com>
2016-12-15 15:52:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,26 +2,22 @@ var SlackBot = require('slackbots')
  , http = require('http')
  , req = require('request')
  , fs = require('fs')
- , channel = '<YOUR SLACK CHANNEL>'
+ , channel = '<YOUR CHANNEL>'
  , request = require('request')
-
+ , alex = require('alex')
 var bot = new SlackBot({
-    token: '<YOUR SLACK API KEY>',
+    token: '<YOUR API KEY>',
     name: 'not_poindexter'
 })
-
 function isMsg(data) {
     if (data.type == "message") { return true } else { return false }
 }
-
 var params = { icon_emoji: ':apace:' }
-
 var download = function(uri, filename, callback){
   request.head(uri, function(err, res, body){
     request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
   });
 };
-
 bot.on('start', function(err, data) {
     if (err) {
         console.log('Failed to connect to Slack server.')
@@ -31,19 +27,18 @@ bot.on('start', function(err, data) {
         }
     }
 })
-
 bot.on('message', function(data, input) {
-    if (isMsg(data)) {
-        if (data.text == '.rquote') {
+        if (data.text.toLowerCase() == '.rquote') {
             require('http').get('http://inspirobot.me/api?generate=true', (res) => {
                 res.setEncoding('utf8');
                 res.on('data', function (body) {
                     bot.postMessageToChannel(channel, body, params)
-					download(body, 'img/' + body.split("http://generated.inspirobot.me/")[1].split("/")[1], function(){
-						console.log("File written to", 'img/' + body.split("http://generated.inspirobot.me/")[1].split("/")[1], "requested by User", data.user, "at", Date())
-					});
+                    console.log("Inspiration deployed to", data.user, "at", Date(), "image", body)
                 });
             });
         }
     }
+})
+bot.on('close', function(err, data) {
+    console.log('Bot is dying.')
 })
