fc5f90a92959271d57d169dba69359e548b7bb3c
Dave <dhurley@singlehop.com>
2016-12-22 15:42:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 var SlackBot = require('slackbots')
  , http = require('http')
  , req = require('request')
- , fs = require('fs')
  , channel = '<YOUR CHANNEL>'
  , request = require('request')
  , alex = require('alex')
+
 var bot = new SlackBot({
     token: '<YOUR API KEY>',
     name: 'not_poindexter'
 })
+
 function isMsg(data) {
     if (data.type == "message") { return true } else { return false }
 }
+
 var params = { icon_emoji: ':apace:' }
-var download = function(uri, filename, callback){
-  request.head(uri, function(err, res, body){
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-  });
-};
+
 bot.on('start', function(err, data) {
     if (err) {
         console.log('Failed to connect to Slack server.')
@@ -27,6 +25,7 @@ bot.on('start', function(err, data) {
         }
     }
 })
+
 bot.on('message', function(data, input) {
         if (data.text.toLowerCase() == '.rquote') {
             require('http').get('http://inspirobot.me/api?generate=true', (res) => {
@@ -39,6 +38,7 @@ bot.on('message', function(data, input) {
         }
     }
 })
+
 bot.on('close', function(err, data) {
     console.log('Bot is dying.')
 })
