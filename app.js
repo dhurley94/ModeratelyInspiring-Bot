c9664e0e48de30a4cc57ff72ae33586e805e364e
Dave Hurley <dhurley@dhurley-laptop.local>
2017-08-28 06:38:27 -0400

    thins

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var SlackBot = require('slackbots');
-var http = require('http');
-var req = require('request');
-var request = require('request');
+var SlackBot = require('slackbots')
+var http = require('http')
+var req = require('request')
+var request = require('request')
 var alex = require('alex');
 
 var channel = ('<SLACK-CHANNEL>');
@@ -9,7 +9,7 @@ var channel = ('<SLACK-CHANNEL>');
 var bot = new SlackBot({
    token: '<WEB TOKEN>',
    name: 'InspiroBoterino'
-});
+})
 
 var botParams = {
   icon_emoji: ':fidget_spinner:'
@@ -22,40 +22,38 @@ function isMsg(data) {
 bot.on('start', function(err, data) {
    if (err) {
        console.log('Failed to connect to Slack server.')
-   } else {
-       if (bot.getChannel(channel)) {
-           bot.postMessageToChannel(channel, '', botParams)
-       }
+       throw err
    }
-});
+})
 
 bot.on('message', function(data, input) {
-    if (data.text == '.inspire') {
-        require('http').get('http://inspirobot.me/api?generate=true', (res) => {
-            res.setEncoding('utf8');
-            res.on('data', function (body) {
-                bot.postMessageToChannel(channel, body, botParams)
-                console.log("Inspiration deployed to", data.user, "at", Date(), "image", body)
-               });
-           });
-       }
-       if (data.text == '.rquote') {
-         request.get('http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en', function(error, response, body) {
-           if (error) { throw error }
-           var quote = JSON.parse(body)
-           quote = "> " + quote.quoteText + "\n - " + quote.quoteAuthor;
-           console.log(quote)
-           bot.postMessageToChannel(channel, quote, botParams)
-         });
-       }
-       if (data.text == '.help') {
+  if (data.text == '.inspire') {
+    require('http').get('http://inspirobot.me/api?generate=true', (res) => {
+      res.setEncoding('utf8')
+      res.on('data', function (body) {
+        bot.postMessageToChannel(channel, body, botParams)
+        console.log("Inspiration deployed to", data.user, "at", Date(), "image", body)
+      })
+    })
+  }
+  if (data.text == '.rquote') {
+    request.get('http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en', function(error, response, body) {
+      if (error) { throw error }
+        var quote = JSON.parse(body)
+        quote = "> " + quote.quoteText + "\n - " + quote.quoteAuthor;
+        console.log(quote)
+        bot.postMessageToChannel(channel, quote, botParams)
+  })
+  }
+  if (data.text == '.help') {
          bot.postMessageToChannel(channel, "No.", botParams)
-       }
-       if (data.text == '.src') {
-         bot.postMessageToChannel(channel, "https://raw.githubusercontent.com/dhurley94/not_poindexter/master/app.js", botParams)
-       }
-});
+  }
+  if (data.text == '.src') {
+    bot.postMessageToChannel(channel, "https://github.com/dhurley94/ModeratelyInspiring-Bot", botParams)
+  }
+})
 
 bot.on('close', function(err, data) {
+   console.log(err)
    console.log('Bot is dying.')
-});
+})
