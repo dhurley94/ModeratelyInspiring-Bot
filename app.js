a151f1394c9082e709fdd65b466b15ba77fab2b4
dhurley94 <drhurley94@gmail.com>
2017-11-27 07:02:09 -0500

    added drunkspire

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var keys = require('./configs/authentication');
 var channel = (keys.slack.channel);
 
 var bot = new SlackBot({
-   token: keys.slack.token,
-   name: keys.slack.name
+  token: keys.slack.token,
+  name: keys.slack.name
 })
 
 var botParams = {
@@ -17,17 +17,17 @@ var botParams = {
 }
 
 function isMsg(data) {
-   if (data.type === "message") { return true } else { return false }
+  if (data.type === "message") { return true } else { return false }
 }
 
-bot.on('start', function(err, data) {
-   if (err) {
-       console.log('Failed to connect to Slack server.')
-       throw err
-   }
+bot.on('start', function (err, data) {
+  if (err) {
+    console.log('Failed to connect to Slack server.')
+    throw err
+  }
 })
 
-bot.on('message', function(data, input) {
+bot.on('message', function (data, input) {
   if (data.text === '.inspire') {
     require('http').get('http://inspirobot.me/api?generate=true', (res) => {
       res.setEncoding('utf8')
@@ -38,46 +38,42 @@ bot.on('message', function(data, input) {
     })
   }
   if (data.text === '.rquote') {
-    request.get('http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en', function(error, response, body) {
+    request.get('http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en', function (error, response, body) {
       if (error) { throw error }
-        var quote = JSON.parse(body)
-        quote = "> " + quote.quoteText + "\n - " + quote.quoteAuthor;
-        console.log(quote)
-        bot.postMessageToChannel(channel, quote, botParams)
+      var quote = JSON.parse(body)
+      quote = "> " + quote.quoteText + "\n - " + quote.quoteAuthor;
+      console.log(quote)
+      bot.postMessageToChannel(channel, quote, botParams)
     })
   }
-  if(data.text === '.drunkspire') {
-    let auth_secrets =  {
-       id: keys.imgur.id,
-       secret: keys.imgur.secret
+  if (data.text === '.drunkspire') {
+    var auth_secrets = {
+      id: keys.imgur.id,
+      secret: keys.imgur.secret
     }
-    let album = keys.imgur.album;
-
     request.get({
-      headers: {
-        auth_secrets
-      },
-      album,
-      method="GET"
+      uri: keys.imgur.uri,
+      authorization: auth_secrets
     },
-    function(error, response, body) {
-      if (error) throw new error;
-      console.log(response, body);
+    function (error, response, body) {
+      var drunk = JSON.parse(body);
+      drunk.images.forEach(element => {
+        element
+      });
     });
-
   }
   if (data.text === '.help') {
     bot.postMessageToChannel(channel, "No.", botParams)
   }
   if (data.text === '.help') {
-         bot.postMessageToChannel(channel, "No.", botParams)
+    bot.postMessageToChannel(channel, "No.", botParams)
   }
   if (data.text === '.src') {
     bot.postMessageToChannel(channel, "https://github.com/dhurley94/ModeratelyInspiring-Bot", botParams)
   }
 })
 
-bot.on('close', function(err, data) {
-   console.log(err)
-   console.log('Bot is dying.')
+bot.on('close', function (err, data) {
+  console.log(err)
+  console.log('Bot is dying.')
 })
